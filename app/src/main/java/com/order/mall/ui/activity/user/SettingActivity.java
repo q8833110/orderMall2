ce0275ced6685a3af8a66ec1d98c8303a1c29554
q8833110 <625404879@qq.com>
2019-05-12 19:06:47 +0800

    文件上传

--- app/src/main/java/com/order/mall/ui/activity/user/SettingActivity.java
+++ app/src/main/java/com/order/mall/ui/activity/user/SettingActivity.java
@@ -33,14 +33,12 @@ import com.order.mall.model.netword.ApiResult;
 import com.order.mall.ui.BaseActivity;
 import com.order.mall.ui.adapter.AddressAdapter;
 import com.order.mall.ui.widget.Dialog.ActionSheetDialog;
-import com.order.mall.util.FileUploadUtils;
 import com.order.mall.util.RetrofitUtils;
 import com.order.mall.util.VersionUtils;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -101,9 +99,10 @@ public class SettingActivity extends BaseActivity {
 
     private static final int REQUEST_CODE_CAMERA = 0x1103;
     private static final int REQUEST_CODE_PICK = 0x1104;
-    private static final int REQUEST_CODE_CROP = 0x1105 ;
+    private static final int REQUEST_CODE_CROP = 0x1105;
 
-    private IBaseApi baseApi ;
+    private IBaseApi baseApi;
+
     @Override
     protected void initImmersionBar() {
         ImmersionBar.with(this)
@@ -182,63 +181,93 @@ public class SettingActivity extends BaseActivity {
                                         }
                                     }));
             actionSheetDialog.show();
-        }else{
+        } else {
             actionSheetDialog.show();
         }
+    }
+
+    File outputImage;
+
+    private void selectPhoto() {
+        mPickImageUri = getImageUri("pick");
+        Intent intent = new Intent(Intent.ACTION_PICK, null);
+        intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
+        startActivityForResult(intent, REQUEST_CODE_PICK);
+    }
+
+    private void takePhoto() {
+        mPhotoImageUri = getImageUri("photo");
+        //调用相机
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION | Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, mPhotoImageUri);
+        startActivityForResult(intent, REQUEST_CODE_CAMERA);
+    }
+
+    /**
+     * @param activity    当前activity
+     * @param orgUri      剪裁原图的Uri
+     * @param desUri      剪裁后的图片的Uri
+     * @param aspectX     X方向的比例
+     * @param aspectY     Y方向的比例
+     * @param width       剪裁图片的宽度
+     * @param height      剪裁图片高度
+     * @param requestCode 剪裁图片的请求码
+     */
+    public static void cropImageUri(Activity activity, Uri orgUri, Uri desUri, int aspectX, int aspectY, int width, int height, int requestCode) {
+        Intent intent = new Intent("com.android.camera.action.CROP");
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
         }
-        File outputImage;
-        private void selectPhoto() {
-            mPickImageUri = getImageUri("pick");
-            Intent intent = new Intent(Intent.ACTION_PICK, null);
-            intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
-            startActivityForResult(intent, REQUEST_CODE_PICK);
+        intent.setDataAndType(orgUri, "image/*");
+        intent.putExtra("crop", "true");
+        intent.putExtra("aspectX", aspectX);
+        intent.putExtra("aspectY", aspectY);
+        intent.putExtra("outputX", width);
+        intent.putExtra("outputY", height);
+        intent.putExtra("scale", true);
+        //将剪切的图片保存到目标Uri中
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, desUri);
+        intent.putExtra("return-data", false);
+        intent.putExtra("outputFormat", Bitmap.CompressFormat.JPEG.toString());
+        intent.putExtra("noFaceDetection", true);
+        activity.startActivityForResult(intent, requestCode);
+    }
+
+    private Uri getImageUri(String fileName) {
+        String filePath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/orderMall/";
+        File path = new File(filePath);
+        if (!path.exists()) {
+            boolean isPathSuccess = path.mkdirs();
+            if (!isPathSuccess) {
+                showToast("没有SD卡权限");
+                return null;
+            }
         }
-
-        private void takePhoto () {
-            mPhotoImageUri = getImageUri("photo");
-            //调用相机
-            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION | Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
-            intent.putExtra(MediaStore.EXTRA_OUTPUT, mPhotoImageUri);
-            startActivityForResult(intent, REQUEST_CODE_CAMERA);
-        }
-
-
-
-        private Uri getImageUri (String fileName){
-            String filePath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/orderMall/";
-            File path = new File(filePath);
-            if (!path.exists()) {
-                boolean isPathSuccess = path.mkdirs();
-                if (!isPathSuccess) {
+        Uri imageUri;
+        outputImage = new File(path, fileName + System.currentTimeMillis() + ".jpg");
+        try {
+            if (outputImage.exists()) {
+                boolean isDeleteSuccess = outputImage.delete();
+                if (!isDeleteSuccess) {
                     showToast("没有SD卡权限");
                     return null;
                 }
             }
-            Uri imageUri;
-            outputImage = new File(path, fileName + System.currentTimeMillis() + ".jpg");
-            try {
-                if (outputImage.exists()) {
-                    boolean isDeleteSuccess = outputImage.delete();
-                    if (!isDeleteSuccess) {
-                        showToast("没有SD卡权限");
-                        return null;
-                    }
-                }
-                if (!outputImage.createNewFile()) {
-                    showToast("没有SD卡权限");
-                    return null;
-                }
-            } catch (IOException e) {
-                Log.e(TAG, e.getMessage());
+            if (!outputImage.createNewFile()) {
+                showToast("没有SD卡权限");
+                return null;
             }
-            if (Build.VERSION.SDK_INT >= 24) {
-                imageUri = FileProvider.getUriForFile(this,  "com.order.mall", outputImage);
-            } else {
-                imageUri = Uri.fromFile(outputImage);
-            }
-            return imageUri;
+        } catch (IOException e) {
+            Log.e(TAG, e.getMessage());
         }
+        if (Build.VERSION.SDK_INT >= 24) {
+            imageUri = FileProvider.getUriForFile(this, "com.order.mall", outputImage);
+        } else {
+            imageUri = Uri.fromFile(outputImage);
+        }
+        return imageUri;
+    }
 
     private Uri getCropUri(String fileName) {
         String filePath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/xiaolian/";
@@ -262,72 +291,74 @@ public class SettingActivity extends BaseActivity {
         }
         return Uri.fromFile(outputImage);
     }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == RESULT_OK){
-            if (requestCode == REQUEST_CODE_CAMERA){
-                 String fileName = outputImage.getAbsolutePath() ;
-                 uploadFile(fileName);
-            }else if (requestCode == REQUEST_CODE_PICK){
-                String pickImagePath = getRealPathFromUri(this ,data.getData());
-                uploadFile(pickImagePath);
+        int output_X = 480, output_Y = 480;
+        if (resultCode == RESULT_OK) {
+            if (requestCode == REQUEST_CODE_CAMERA) {
+//                 String fileName = outputImage.getAbsolutePath() ;
+                mCropImageUri = getCropUri("crop");
+                cropImageUri(this, mPhotoImageUri, mCropImageUri, 1, 1, output_X, output_Y, REQUEST_CODE_CROP);
+//                 uploadFile(fileName);
+            } else if (requestCode == REQUEST_CODE_PICK) {
+                mCropImageUri = getCropUri("crop");
+//                Uri newUri = Uri.parse(PhotoUtils.getPath(this, data.getData()));
+                cropImageUri(this, data.getData(), mCropImageUri, 1, 1, output_X, output_Y, REQUEST_CODE_CROP);
+//                String pickImagePath = getRealPathFromUri(this ,data.getData());
+//                uploadFile(pickImagePath);
+            } else if (requestCode == REQUEST_CODE_CROP) {
+//                String cropImagePath = getRealPathFromUri(this ,data.getData());
+                String photoPath = getRealPathFromUri(this, mCropImageUri);
+                uploadFile(photoPath);
             }
         }
     }
 
-    public void uploadFile(String  fileName){
-            File file = new File(fileName);
-            if (!file.exists()) return ;
-        RequestBody requestBody ;
-        MultipartBody.Builder builder ;
-        builder= new MultipartBody.Builder().setType(MultipartBody.FORM)
-               .addFormDataPart("imgs",file.getName() ,RequestBody.create(MediaType.parse("image/*") ,file));
+    public void uploadFile(String fileName) {
+        File file = new File(fileName);
+        if (!file.exists()) return;
+        RequestBody requestBody;
+        MultipartBody.Builder builder;
+        builder = new MultipartBody.Builder().setType(MultipartBody.FORM)
+                .addFormDataPart("imgs", file.getName(), RequestBody.create(MediaType.parse("image/*"), file));
         requestBody = builder.build();
-        addObserver(baseApi.upload(requestBody),new NetworkObserver<ApiResult<String>>(){
-                @Override
-                public void onReady(ApiResult<String> stringApiResult) {
-                        if (stringApiResult != null){
-
-                        }else{
-                            showToast(stringApiResult.getMessage());
-                        }
+        addObserver(baseApi.upload(requestBody), new NetworkObserver<ApiResult<List<String>>>() {
+            @Override
+            public void onReady(ApiResult<List<String>> stringApiResult) {
+                if (stringApiResult.getData() != null && stringApiResult.getData().size() > 0) {
+                    List<String> urls = stringApiResult.getData();
+                    Glide.with(SettingActivity.this).load(urls.get(0)).into(ivHead);
+                    showToast("修改头像成功");
+                } else {
+                    showToast(stringApiResult.getMessage());
                 }
-            });
+            }
+        });
     }
-
 
 
     /**
      * 根据图片的Uri获取图片的绝对路径。@uri 图片的uri
-     * @return 如果Uri对应的图片存在,那么返回该图片的绝对路径,否则返回null
+     *
+     * @return 如果Uri对应的图片存在, 那么返回该图片的绝对路径, 否则返回null
      */
     public static String getRealPathFromUri(Context context, Uri uri) {
-        if(context == null || uri == null) {
+        if (context == null || uri == null) {
             return null;
         }
-        if("file".equalsIgnoreCase(uri.getScheme())) {
-            return getRealPathFromUri_Byfile(context,uri);
-        } else if("content".equalsIgnoreCase(uri.getScheme())) {
-            return getRealPathFromUri_Api11To18(context,uri);
+        if ("file".equalsIgnoreCase(uri.getScheme())) {
+            return getRealPathFromUri_Byfile(context, uri);
+        } else if ("content".equalsIgnoreCase(uri.getScheme())) {
+            return getRealPathFromUri_Api11To18(context, uri);
         }
-//        int sdkVersion = Build.VERSION.SDK_INT;
-//        if (sdkVersion < 11) {
-//            // SDK < Api11
-//            return getRealPathFromUri_BelowApi11(context, uri);
-//        }
-////        if (sdkVersion < 19) {
-////             SDK > 11 && SDK < 19
-////            return getRealPathFromUri_Api11To18(context, uri);
-//            return getRealPathFromUri_ByXiaomi(context, uri);
-////        }
-//        // SDK > 19
         return getRealPathFromUri_AboveApi19(context, uri);//没用到
     }
 
 
     //针对图片URI格式为Uri:: file:///storage/emulated/0/DCIM/Camera/IMG_20170613_132837.jpg
-    private static String getRealPathFromUri_Byfile(Context context,Uri uri){
+    private static String getRealPathFromUri_Byfile(Context context, Uri uri) {
         String uri2Str = uri.toString();
         String filePath = uri2Str.substring(uri2Str.indexOf(":") + 3);
         return filePath;
@@ -346,9 +377,9 @@ public class SettingActivity extends BaseActivity {
         // 使用':'分割
         String id = wholeID.split(":")[1];
 
-        String[] projection = { MediaStore.Images.Media.DATA };
+        String[] projection = {MediaStore.Images.Media.DATA};
         String selection = MediaStore.Images.Media._ID + "=?";
-        String[] selectionArgs = { id };
+        String[] selectionArgs = {id};
 
         Cursor cursor = context.getContentResolver().query(
                 MediaStore.Images.Media.EXTERNAL_CONTENT_URI, projection,
@@ -368,7 +399,7 @@ public class SettingActivity extends BaseActivity {
      */
     private static String getRealPathFromUri_Api11To18(Context context, Uri uri) {
         String filePath = null;
-        String[] projection = { MediaStore.Images.Media.DATA };
+        String[] projection = {MediaStore.Images.Media.DATA};
 
         CursorLoader loader = new CursorLoader(context, uri, projection, null,
                 null, null);
